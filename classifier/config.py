f56e5edcbb7d863c3147b9aa291ce5fec834e1ca
Ajay Nair <prodigyaj@gmail.com>
2014-12-10 01:32:48 -0600

    Initial Commit with some files. Not completely working off the box

---
+++ classifier/config.py
@@ -0,0 +1,3 @@
+training_data_location = "/srv/data/anair10/cs598/data/webkb/"
+extract_output_location = "/srv/data/anair10/cs598/data/extract_text/"
+beautiful_soup_path = "/srv/data/anair10/cs598/code/"
