ca68f8e270dea1c0f6ee58db3f660ecb37ea5803
Ajay Nair <prodigyaj@gmail.com>
2014-12-11 02:32:33 -0600

    Train and Predict files for learning. The code takes care of saving the classifier and category mapping in a specified config file

--- classifier/config.py
+++ classifier/config.py
@@ -1,3 +1,12 @@
-training_data_location = "/srv/data/anair10/cs598/data/webkb/"
-extract_output_location = "/srv/data/anair10/cs598/data/extract_text/"
-beautiful_soup_path = "/srv/data/anair10/cs598/code/"
+training_data_location = "/home/anair10/topic/webkb/"
+extract_output_location = "/home/anair10/topic/extract_text/"
+beautiful_soup_path = "/home/anair10/topic/"
+learning_data_train = "/home/anair10/topic/train/"
+learning_data_test = "/home/anair10/topic/test/"
+
+export_classifier_location = "/home/anair10/topic/trained_classifier/"
+import_classifier_location = "/home/anair10/topic/trained_classifier/"
+
+
+#FILENAMES
+category_filename = "category_def_file"
